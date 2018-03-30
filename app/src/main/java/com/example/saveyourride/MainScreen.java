18db953cf3c3c1a43cc70d9893b3580ad27bcade
taraszaika <taraszajika@gmail.com>
2018-03-30 15:49:00 +0200

    new Version + SDK

--- app/src/main/java/com/example/saveyourride/MainScreen.java
+++ app/src/main/java/com/example/saveyourride/MainScreen.java
@@ -1,10 +1,11 @@
 package com.example.saveyourride;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+
+import java.util.List;
 
 
 public class MainScreen extends AppCompatActivity {
@@ -13,8 +14,22 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-
+        final SensorManager mSensorManager;
+        final Sensor mAccelerometer;
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        print(mSensorManager);
     }
 
-    // HALLO Pascal
+    private void print(SensorManager m) {
+        int i = 0;
+        while (i < 2) {
+            List<Sensor> list = m.getSensorList(Sensor.TYPE_ALL);
+            System.out.println("Hallo");
+            for (Sensor each: list) {
+                System.out.println(each.getName());
+            }
+            i++;
+        }
+    }
 }
