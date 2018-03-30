aab70e4f856881a153c5b01af4a52ae71de93432
taraszaika <taraszajika@gmail.com>
2018-03-30 17:42:53 +0200

    Settings Button

--- app/src/main/java/com/example/saveyourride/MainScreen.java
+++ app/src/main/java/com/example/saveyourride/MainScreen.java
@@ -1,9 +1,13 @@
 package com.example.saveyourride;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.List;
 
@@ -34,4 +38,27 @@ public class MainScreen extends AppCompatActivity {
     }
 
     // New Branch //
+
+    // Edit Pascal
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //menu activity bekannt
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //click listener quasi.
+
+        switch (item.getItemId()) {
+            case R.id.settingsButton:
+                System.out.println("SETTINGS");
+                // Intent erzeugen und Starten der AktiendetailActivity mit explizitem Intent
+                Intent settingsIntent = new Intent(this, MainScreen.class);
+                // settingsIntent.putExtra(Intent.EXTRA_TEXT, aktienInfo);
+                startActivity(settingsIntent);
+                break;
+        }
+        return super.onOptionsItemSelected(item); //To change body of generated methods, choose Tools | Templates.
+    }
 }
