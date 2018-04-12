d411566539702a49b1d57e3453f813390bf136c6
taraszaika <taraszajika@gmail.com>
2018-04-12 11:40:10 +0200

    CountDownTimer new implementation in active Fragment

--- app/src/main/java/com/example/saveyourride/fragments/Active.java
+++ app/src/main/java/com/example/saveyourride/fragments/Active.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.saveyourride.R;
 import com.example.saveyourride.activities.MainScreen;
@@ -21,141 +22,47 @@ import com.example.saveyourride.services.IntervallTimer;
 
 public class Active extends Fragment {
 
-    final int maxIntervalls = 6;
-    final int intervallTime = 10000;
-    int intervallCounter = 0;
-    boolean timerStart = true;
-
-    Button button;
-
-    //BroadcastReceiver for ActiveFragment
-    BroadcastReceiver mReceiver;
-    IntentFilter intentFilter = new IntentFilter(
-            "android.intent.action.FINISHED");
-
-    private Handler customHandler = new Handler();
+    public int counter;
+    public long totalTime, interval;
+    private Button buttonStartTimer;
+    private Button buttonStopTimer;
+    private TextView textViewTimerCount;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View fragmentView = inflater.inflate(R.layout.fragment_active, container, false);
 
-        //get Views
-        button = (Button) fragmentView.findViewById(R.id.button);
-        ///
+        buttonStartTimer = (Button) fragmentView.findViewById(R.id.buttonStartTimer);
+        buttonStopTimer = (Button) fragmentView.findViewById(R.id.buttonStopTimer);
+        textViewTimerCount = (TextView) fragmentView.findViewById(R.id.textViewTimerCount);
 
-        button.setText("Starten");
-        button.setBackgroundColor(Color.GRAY);
+        totalTime = 10000;
+        interval = 1000;
 
-
-
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonStartTimer.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                new CountDownTimer(totalTime, interval) {
+                    public void onTick(long millisUntilFinished) {
+                        textViewTimerCount.setText(String.valueOf(counter));
+                        counter++;
+                    }
 
-                if (timerStart) {
-//                    System.out.println("Custom Handler - Button");
-//                    intervallCounter = 0;
-//                    timerStart = false;
-//                    customHandler.post(intervallThread);
-                    getActivity().startService(new Intent(getActivity(), IntervallTimer.class));
-                    time();
-
-                } else resetTimer();
+                    public void onFinish() {
+                        textViewTimerCount.setText("FINISH!!");
+                    }
+                }.start();
             }
         });
-
-
-        //BroadcastReceiver for ActiveFraagment
-
-
-        mReceiver = new BroadcastReceiver() {
-
+        buttonStopTimer.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onReceive(Context context, Intent intent) {
-                //extract our message from intent
-                String msg_for_me = intent.getStringExtra("some_msg");
-                System.out.println("Fragment-Receiver: "+ msg_for_me);
-
-                button.setText(msg_for_me);
-
-
+            public void onClick(View v) {
+                /// NICHTS PASSIERT
             }
-        };
-
-        //registering our receiver
-        getActivity().registerReceiver(mReceiver, intentFilter);
-
-        ///End - BroadcastReceiver for ActiveFragment
+        });
 
         return fragmentView;
     }
 
-    private Runnable intervallThread = new Runnable() {
-
-        public void run() {
-
-            if (intervallCounter < maxIntervalls) {
-                intervallBenachrichtigung(intervallCounter);
-                intervallCounter++;
-                customHandler.postDelayed(this, intervallTime);
-
-
-            } else {
-                System.out.println("Notruf");
-                button.setText("Notruf");
-            }
-
-        }
-    };
-
-    private void intervallBenachrichtigung(int intervallCounter) {
-        int übrigeIntervalle = maxIntervalls - intervallCounter;
-        System.out.println("übrige Intervalle: " + übrigeIntervalle);
-        if (intervallCounter < 2)
-            button.setBackgroundColor(Color.GREEN);
-        else if (übrigeIntervalle <= 2)
-            button.setBackgroundColor(Color.RED);
-        else
-            button.setBackgroundColor(Color.YELLOW);
-
-        button.setText(intervallCounter + " / " + maxIntervalls);
-    }
-
-    public void resetTimer() {
-        customHandler.removeCallbacks(intervallThread);
-        intervallCounter = 0;
-        customHandler.post(intervallThread);
-
-    }
-
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        //unregister our receiver
-       // getActivity().unregisterReceiver(this.mReceiver);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        //register our receiver
-        getActivity().registerReceiver(mReceiver, intentFilter);
-    }
-
-    public void time(){
-        new CountDownTimer(300000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                System.out.println("seconds remaining: " + millisUntilFinished / 1000);
-                //here you can have your logic to set text to edittext
-            }
-
-            public void onFinish() {
-                System.out.println("done!");
-            }
-
-        }.start();
-    }
 }
