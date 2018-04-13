1aa291a939d85f01eb058876a1e25ae9d932c70c
taraszaika <taraszajika@gmail.com>
2018-04-13 16:52:16 +0200

    Passive Mode Init. Activity and Map added.

---
+++ app/src/main/java/com/example/saveyourride/activities/PassiveMode.java
@@ -0,0 +1,35 @@
+package com.example.saveyourride.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.saveyourride.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class PassiveMode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passive_mode);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapInPassiveMode);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                LatLng latLng = new LatLng(51.023226, 7.564927);
+                googleMap.addMarker(new MarkerOptions().position(latLng)
+                        .title("Singapore"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+                googleMap.getUiSettings().setAllGesturesEnabled(false);
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/saveyourride/fragments/Passive.java
+++ app/src/main/java/com/example/saveyourride/fragments/Passive.java
@@ -1,12 +1,15 @@
 package com.example.saveyourride.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.saveyourride.R;
+import com.example.saveyourride.activities.PassiveMode;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,6 +20,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Passive extends Fragment{
 
     private SupportMapFragment mapFragment;
+    private Button buttonStartPassiveMode;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,7 +35,7 @@ public class Passive extends Fragment{
                     LatLng latLng = new LatLng(51.023226, 7.564927);
                     googleMap.addMarker(new MarkerOptions().position(latLng)
                             .title("Singapore"));
-                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
                     googleMap.getUiSettings().setAllGesturesEnabled(false);
                 }
             });
@@ -39,6 +43,17 @@ public class Passive extends Fragment{
 
         // R.id.map is a FrameLayout, not a Fragment
         getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
+
+        buttonStartPassiveMode = (Button) rootView.findViewById(R.id.buttonStartPassiveMode);
+
+        buttonStartPassiveMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("PASSIVE MODE ACTIVITY");
+                Intent passiveModeIntent = new Intent(getActivity(), PassiveMode.class);
+                startActivity(passiveModeIntent);
+            }
+        });
 
         return rootView;
     }
