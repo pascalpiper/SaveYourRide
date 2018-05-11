8c463807ad00e7bad2ae655a0463ac81075935f5
pascal <pascal-piper@gmx.de>
2018-05-11 10:59:17 +0200

    send sms

--- app/src/main/java/com/saveyourride/activities/Activity_Active.java
+++ app/src/main/java/com/saveyourride/activities/Activity_Active.java
@@ -13,74 +13,14 @@ import android.widget.TextView;
 import com.saveyourride.R;
 
 
-    public class Activity_Active extends Activity implements View.OnClickListener {
-        /** Called when the activity is first created. */
-        AnimationDrawable mFrameAnimation = null;
+    public class Activity_Active extends Activity {
 
-        boolean mbUpdating = false;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity__active);
 
-            Button btnWheel = (Button)findViewById(R.id.wheel_button);
-            btnWheel.setOnClickListener(this);
-
-            mFrameAnimation = (AnimationDrawable) btnWheel.getBackground();
         }
 
-        public void onClick(View v) {
-            if(v.getId() == R.id.wheel_button) {
-                if(!mbUpdating) {
-                    mbUpdating = true;
-                    new AsyncTaskForUpdateDB().execute("");
-                }
-            }
-
-        }
-
-        private class AsyncTaskForUpdateDB extends AsyncTask<String, Integer, ResultOfAsyncTask> {
-
-            @Override
-            protected void onPreExecute() {
-
-                mFrameAnimation.start();
-                super.onPreExecute();
-            }
-
-            @Override
-            protected ResultOfAsyncTask doInBackground(String... strData) {
-                ResultOfAsyncTask result = new ResultOfAsyncTask();
-
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                return result;
-            }
-
-            @Override
-            protected void onPostExecute(ResultOfAsyncTask result) {
-                mFrameAnimation.stop();
-                mbUpdating = false;
-            }
-
-            @Override
-            protected void onCancelled() {
-                mFrameAnimation.stop();
-                mbUpdating = false;
-                super.onCancelled();
-            }
-
-            @Override
-            protected void onProgressUpdate(Integer... progress) {
-            }
-        }
-
-        private class ResultOfAsyncTask {
-            int iErrorCode = 0;
-        }
     }
