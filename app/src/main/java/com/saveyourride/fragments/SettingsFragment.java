722ba7a8ffae21355ad1871e7505379042bfed49
pascal <pascal-piper@gmx.de>
2018-05-18 13:25:28 +0200

    commit

--- app/src/main/java/com/saveyourride/fragments/SettingsFragment.java
+++ app/src/main/java/com/saveyourride/fragments/SettingsFragment.java
@@ -3,6 +3,7 @@ package com.saveyourride.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.saveyourride.R;
@@ -22,6 +23,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState,
                                     String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+        getPreferenceManager().getPreferenceScreen().findPreference("pref_gps").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if (getPreferenceManager().getPreferenceScreen().findPreference("pref_name").isEnabled()) {
+                    getPreferenceManager().getPreferenceScreen().findPreference("pref_name").setEnabled(false);
+                } else {
+                    getPreferenceManager().getPreferenceScreen().findPreference("pref_name").setEnabled(true);
+
+                }
+                return false;
+            }
+        });
     }
 
 }
